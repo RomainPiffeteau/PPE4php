77abe456f13a9e581a2a08d92b3556b0c303ff55
ypçter <et.bourreau@laposte.net>
2017-03-07 11:54:51 +0100

    base projet

---
+++ vues/v_connexion.php
@@ -0,0 +1,32 @@
+﻿<div id="contenu">
+    <h2>Identification utilisateur</h2>
+	<form method="POST" action="index.php?uc=connexion&action=valideConnexion">
+		<table style="border: 0">
+			<tr>
+				<td style="text-align: right; border: 0;">
+					<label for="nom">Login*</label>
+				</td>
+				<td style="border: 0">
+					<input id="login" type="text" name="login"  size="30" maxlength="45">
+				</td>
+			</tr>
+			<tr>
+				<td style="text-align: right; border: 0;">
+					<label for="mdp">Mot de passe*</label>
+				</td>
+				<td style="border: 0">
+					<input id="mdp"  type="password"  name="mdp" size="30" maxlength="45">
+				</td>
+			</tr>
+			<tr>
+				<td style="text-align: right; border: 0;">
+					<input type="submit" value="Valider" name="valider">
+				</td>
+				<td style="border: 0">
+					<input type="reset" value="Annuler" name="annuler"> 
+				</td>
+			</tr>
+		</table>
+	</form>
+
+</div>
---
+++ vues/v_sommaire.php
@@ -0,0 +1,36 @@
+﻿<!-- Division pour le sommaire -->
+<div id="menuGauche">
+	<div id="infosUtil">
+
+		<h2>
+			MENU
+		</h2>
+
+	</div>  
+	<ul id="menuList">
+	<li >
+	<?php
+	$grade = $pdo->getGrade();
+	Visiteur :
+	
+	<br>
+	<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
+	</li>
+	<li class="smenu">
+	<a href="index.php?uc=gererFrais&action=saisirFrais" title="Saisie fiche de frais ">Saisie fiche de frais</a>
+	</li>
+	<li class="smenu">
+	<a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
+	</li>
+	<li class="smenu">
+	<a href="index.php?uc=reparation&action=ajouter" title="Faire une demande de réparation">Faire une demande de réparation</a>
+	</li>
+	<li class="smenu">
+	<a href="index.php?uc=reparation&action=visualiser" title="Visualiser mes demandes de réparation">Visualiser mes demandes de réparation</a>
+	</li>
+	<li class="smenu">
+	<a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
+	</li>
+	</ul>
+
+</div>
