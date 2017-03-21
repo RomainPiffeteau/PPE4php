daa89c3515d42364e2e578e3c359c50e7101fbf5
ypçter <et.bourreau@laposte.net>
2017-03-21 08:05:33 +0100

    reparation controlle fix

--- controleurs/c_reparation.php
+++ controleurs/c_reparation.php
@@ -3,24 +3,22 @@ include("vues/v_sommaire.php");
 $action = $_REQUEST['action'];
 $idVisiteur = $_SESSION['idVisiteur'];
 switch($action){
-	case 'visualiser':{
+	case 'visualiser':
 		$mesReparations=$pdo->getReparations($idVisiteur);
 		include("vues/v_visualiserReparations.php");
 		break;
-	}
 	case 'prisesEnCharge':
 		$mesPersonnesACharge = $pdo->getPersonnesACharge($_SESSION['idVisiteur']);
 		include("vues/v_reparationsPersonnesACharge.php");
 		break;
-	case 'ajouter':{
+	case 'ajouter':
 		//TODO
 		?>
 		Cette Page est en cours de construction
 		<?php
 		break;
 	case 'montantGlobal':
-		include("vues/v_raparationsMontantGlobal.php");
+		include("vues/v_reparationsMontantGlobal.php");
 		break;
-	}
 }
 ?>
