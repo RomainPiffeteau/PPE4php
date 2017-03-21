2966a176d9eefb61270433775747943239ee515a
ypçter <et.bourreau@laposte.net>
2017-03-21 15:01:14 +0100

    css fixes and reparation gestion

--- vues/v_reparationsGestion.php
+++ vues/v_reparationsGestion.php
@@ -39,14 +39,14 @@
 						?>
 						<tr>
 							<td><?=$lineReparation['prenom']?> <?=$lineReparation['nom']?></td>
-							<td><?=$lineReparation['equipement']?></td>
-							<td><?=$lineReparation['typePanne']?></td>
-							<td><?=$lineReparation['jour']?></td>
+							<td><?=$lineReparation['libelle']?></td>
+							<td><?=$lineReparation['naturePanne']?></td>
+							<td><?=$lineReparation['jourDemande']?></td>
 							<td><?php if(empty($lineReparation['prix'])) echo "/"; elseif(empty($lineReparation['majoration'])) echo $lineReparation['prix']."€"; else echo ($lineReparation['prix']*$lineReparation['majoration'])."€" ?></td>
 							<td><?php if(empty($lineReparation['dateFinT'])) echo "/"; else echo $lineReparation['dateFinT'] ?></td>
 							<td><?php if(empty($lineReparation['dateFinR'])) echo "/"; else echo $lineReparation['dateFinR'] ?></td>
 							<td><?php if(empty($lineReparation['commentaire'])) echo "/"; else echo $lineReparation['commentaire'] ?></td>
-							<td><a href="../index.html?uc=reparation&action=validerDemande&id=<?=$lineReparation['id']?>"><img src="../img/valid.gif" alt="Valid" title="Accepter la demande"/></a> <a href="../index.html?uc=reparation&action=annulerDemande&id=<?=$lineReparation['id']?>"><img src="../img/cancel.gif" alt="Cancel" title="Refuser la demande"/></a></td>
+							<td><a href="./index.html?uc=reparation&action=validerDemande&id=<?=$lineReparation['id']?>"><img src="./img/valid.gif" alt="Valid" title="Accepter la demande"/></a> <a href="./index.html?uc=reparation&action=annulerDemande&id=<?=$lineReparation['id']?>"><img src="./img/cancel.gif" alt="Cancel" title="Refuser la demande"/></a></td>
 						</tr>
 						<?php
 					}
