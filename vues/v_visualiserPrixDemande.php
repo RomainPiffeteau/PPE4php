d1038f3765319bc4013cc1ff6a66294c75e34508
RomainPiffeteau <romain.piffeteau@hotmail.fr>
2017-04-17 16:10:19 +0200

    Mise en forme et ajout de commentaires sur v_visualiserPrixDemande.php

--- vues/v_visualiserPrixDemande.php
+++ vues/v_visualiserPrixDemande.php
@@ -2,10 +2,12 @@
 	<h3>Mes Demandes de Réparation</h3>
 	<div class="encadre">
 		<?php
-		// echo $grade['id'];
+		// Vérifie si l'utilisateur est un Délégué ou un responsable
 		if($grade['id'] == 2 || $grade['id'] == 3){
+			// Vérifie si l'utilisateur à bien des répérations qui lui sont associées (répérations provenant de getReparations dans le pdo)
 			if(count($mesReparations) > 0){
 				?>
+				<!-- Affiche le nom des différentes colonnes de la table Panne -->
 					<table class="listeLegere">
 						<tr>
 							<th>Equipement</th>
@@ -21,6 +23,7 @@
 						</tr>
 
 						<?php
+						// Parcours la liste des répérations du visiteur dans la BDD et affiche les données dans un tableau
 						foreach($mesReparations as $lineReparation){
 							?>
 							<tr>
@@ -29,27 +32,30 @@
 								<td><?=$lineReparation['jourDemande']?></td>
 								<td><?php if(empty($lineReparation['prix'])) echo "/"; else echo $lineReparation['prix'].'€'; ?></td>
 								<td><?php if(empty($lineReparation['prix'])) echo "/";
-								else {
-								$prix = $lineReparation['prix'];
-								if(!empty($lineReparation['majoration'])){
-									$prix = $prix + $prix*$lineReparation['majoration']*0.01;
-									echo $prix.'€';
-								}
-								else{
-									echo $prix.'€';
-								}
-							} ?>
-						   </td>
-								<td><?php $dt = new DateTime();
- 										  $dt->format('Y-m-d');
+										else {
+											// Affiche le prix avec l'ajout de la majoration si il y en a une
+											$prix = $lineReparation['prix'];
+											if(!empty($lineReparation['majoration'])){
+												$prix = $prix + $prix*$lineReparation['majoration']*0.01;
+												echo $prix.'€';
+											}
+											else{
+												echo $prix.'€';
+											}
+										} ?>
+						   	</td>
+								<td><?php
+								 			// Compare la date du jour à la date de la demande
+											$dateJour = new DateTime();
+ 										  $dateJour->format('Y-m-d');
 											$date = $lineReparation['jourDemande'];
 											echo $date . "<BR>";
-											$date2 = new DateTime($date);
-									    $interval = $date2->diff($dt);
-											//$interval = date_diff($dt,$date2);
+											$dateDemande = new DateTime($date);
+									    $interval = $dateDemande->diff($dateJour);
 										  $interval = $interval->format('%a');
 										  $intervalValue = intval($interval);
 											echo "Il y a ".$intervalValue .' jour(s) <br/>';
+											// Si la date de la demande date d'il y a plus de 3 jours, demande d'ajout d'une majoration
 											if($intervalValue > 3){
 												$intervalValue=$intervalValue-3;
 												$idPanne = $lineReparation['id'];
@@ -57,18 +63,18 @@
 												entrer une majoration <form name="test" action="" method="post">
 												<input type="number" name="majoration'.$idPanne.'" min="0" max="100">
 												<input type ="submit"> </form>' ;
+												// Effectue l'ajout dans la BDD uniquement si le visiteur à entré une valeur dans le champ
 												if(isset($_POST['majoration'.$idPanne])){
 													try{
+														// Connexion à la BDD
 														$bdd = new PDO('mysql:host=localhost;dbname=gsbjm;charset=utf8', 'root', '');
-														$test = $_POST['majoration'.$idPanne];
-														//echo $test;
-														$bdd->exec('UPDATE panne SET majoration = '.$test.' where id = '.$idPanne);
+														$majoration = $_POST['majoration'.$idPanne];
+														$bdd->exec('UPDATE panne SET majoration = '.$majoration.' where id = '.$idPanne);
 													}
 													catch(Exception $e){
 														die('Erreur : '.$e->getMessage());
 													}
 												}
-
 											}
 										?>
 							 </td>
