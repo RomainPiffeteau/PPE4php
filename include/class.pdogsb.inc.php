ca195acae4a799394b8c884c3f4fe17b7e1f1958
root <root@debianSi7.sio-coliniere44.local>
2017-05-02 09:56:50 +0200

    creating connection vm

--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -18,13 +18,15 @@
 class PdoGsb{
 
       	private static $serveur='sqlsrv:Server=192.168.222.72';
-      	private static $bdd='Database=gsbjm';
-      // private static $user='P2017Piffeteau' ;
-      // private static $user='P2017Bouchez' ;
+      	private static $bdd='dbname=gsbjm';
         private static $user='P2017Bourreau';
       	private static $mdp='Password1';
-		private static $monPdo;
-		private static $monPdoGsb=null;
+	private static $monPdo;
+	private static $monPdoGsb=null;
+	
+        // machine virtuelle
+        // private static $serveur='dblib:host=192.168.222.72';
+        // private static $bdd='dbname=gsbjm';
 
 
     /*   private static $serveur='mysql:host=localhost';
